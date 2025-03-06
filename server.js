18bec0fe898469d6ff396e2b3e4fe036ef55f671
fuenmayore <fuenmayoredwin311@gmail.com>
2025-03-06 18:59:50 -0500

    prueba

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 8080; // ✅ Puerto correcto
 // Verifica que las variables de entorno se carguen correctamente
 console.log("🔹 DB_HOST:", process.env.DB_HOST);
 console.log("🔹 DB_USER:", process.env.DB_USER);
-console.log("🔹 DB_PASS:", process.env.DB_PASS ? "✔ Oculta" : "❌ No encontrada ");
+console.log("🔹 DB_PASS:", process.env.DB_PASS);
 console.log("🔹 DB_NAME:", process.env.DB_NAME);
 console.log("🔹 DB_PORT:", process.env.DB_PORT);
 
