89b03fd21968bb40e9f906bb1e9bb57725b3aacb
fuenmayore <fuenmayoredwin311@gmail.com>
2025-03-06 18:21:13 -0500

    puerto bd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql2");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3306;
 
 // Configurar la base de datos
 const db = mysql.createConnection({
